8024f5402c93ea0292dc078c76e2433594e49763
AleksVitvor <avitvor@gmail.com>
2019-12-13 11:55:16 +0300

    10 ready

---
+++ Vitvor.OOP10/Vitvor.OOP10/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+
+namespace Vitvor.OOP10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+            Random random = new Random();
+            for(short i=0;i<5;i++)
+            {
+                arrayList.Add(random.Next(11, 99));
+            }
+            Console.WriteLine("Первая коллекция из случайных чисел и другого включает: ");
+            arrayList.Add("My substr");
+            arrayList.RemoveAt(3);
+            foreach(var item in arrayList)
+            {
+                Console.WriteLine(item + " ");
+            }
+            Console.WriteLine("Count " + arrayList.Count);
+            Console.WriteLine(arrayList.Contains("My substr"));
+
+            Stack<char> vs = new Stack<char>();
+            Console.WriteLine("Введите количество входных символов");
+            int size = Convert.ToInt32(Console.ReadLine());
+            for(int i=0; i<size; i++)
+            {
+                char sym = Convert.ToChar(Console.ReadLine());
+                vs.Push(sym);
+            }
+            vs.Push('a');
+            Console.WriteLine("Введите колчество элементов для удаления");
+            int toDel = Convert.ToInt32(Console.ReadLine());
+            for(int i=0;i<toDel;i++)
+            {
+                vs.Pop();
+            }
+            foreach (char item in vs)
+                Console.WriteLine(item + " ");
+            List<char> vs1 = new List<char>();
+            for(int i=0; i<vs.Count;i++)
+            {
+                vs1.Add(vs.Pop());
+            }
+            foreach (char item in vs1)
+                Console.WriteLine(item);
+            Console.WriteLine(vs1.Contains('a'));
+
+            Transport first = new Transport(500, 30);
+            Transport second = new Transport(300, 25);
+            Transport third = new Transport(700, 50);
+            Stack<Transport> airline = new Stack<Transport>();
+            airline.Push(first);
+            airline.Push(second);
+            airline.Push(third);
+            foreach (Transport item in airline)
+                Console.WriteLine(item.ToString());
+            int ToDelete = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < ToDelete; i++)
+                airline.Pop();
+            foreach (Transport item in airline)
+                Console.WriteLine(item.ToString());
+            List<Transport> transports = new List<Transport>();
+            for(int i=0; i<airline.Count;i++)
+            {
+                transports.Add(airline.Pop());
+            }
+            foreach (Transport item in transports)
+                Console.WriteLine(item.ToString());
+            ObservableCollection<Transport> observableCollection = new ObservableCollection<Transport>();
+            observableCollection.CollectionChanged += Notification;
+            observableCollection.Add(first);
+            observableCollection.Remove(first);
+            Console.ReadLine();
+        }
+        static void Notification(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            Console.WriteLine("The collection has been changed");
+        }
+
+    }
+}
---
+++ Vitvor.OOP10/Vitvor.OOP10/Transport.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vitvor.OOP10
+{
+    class Transport:IComparable<Transport>
+    {
+        public int Weight { get; set; }
+        public int Capacity { get; set; }
+
+        public int CompareTo(Transport obj)
+        {
+            if (this.Weight > obj.Weight) return 1;
+            if (this.Weight < obj.Weight) return -1;
+            else return 0;
+        }
+        public Transport(int weight, int capacity)
+        {
+            Weight = weight;
+            Capacity = capacity;
+        }
+        public override string ToString()
+        {
+            return $"{Weight} {Capacity}";
+        }
+    }
+    }
+}
